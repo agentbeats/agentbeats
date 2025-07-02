d03704c39c1214e0692457bb6f88097144e69ad0
Daniel Miao <82011817+miaomiaogato@users.noreply.github.com>
2025-07-01 23:29:18 -0700

    changed logger to function through mcp

--- tensortrust_mock_impl/backend/run.py
+++ tensortrust_mock_impl/backend/run.py
@@ -12,6 +12,6 @@ if __name__ == "__main__":
     uvicorn.run(
         "tensortrust_mock_impl.backend.app:app",
         host="0.0.0.0",
-        port=3000,
+        port=3001,
         reload=True
     )
