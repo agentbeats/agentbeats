00a85930a9fda0c5bbf573dee59a0f31d2b6324c
Daniel Miao <82011817+miaomiaogato@users.noreply.github.com>
2025-07-03 22:30:46 -0700

    added frontend

---
+++ frontend/agentbeats/.svelte-kit/generated/client/nodes/7.js
@@ -0,0 +1,3 @@
+import * as universal from "../../../../src/routes/register-agent/+page.ts";
+export { universal };
+export { default as component } from "../../../../src/routes/register-agent/+page.svelte";
---
+++ frontend/agentbeats/.svelte-kit/generated/client/nodes/8.js
@@ -0,0 +1,3 @@
+import * as universal from "../../../../src/routes/stage-battle/+page.ts";
+export { universal };
+export { default as component } from "../../../../src/routes/stage-battle/+page.svelte";
