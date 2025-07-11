cbe3ac5c841e663f30291ad44d0623dd7fb330b7
miaomiaogato <miao.daniel.miao@gmail.com>
2025-07-11 11:01:40 -0400

    changed frontend to fetch from nuggets.puppy9.com instead of localhost

--- frontend/src/lib/api/battles.ts
+++ frontend/src/lib/api/battles.ts
@@ -1,6 +1,6 @@
 export async function createBattle(battleInfo: any) {
 	try {
-		const res = await fetch('http://localhost:9000/battles', {
+		const res = await fetch('http://nuggets.puppy9.com:9000/battles', {
 			method: 'POST',
 			headers: {
 				'Content-Type': 'application/json',
